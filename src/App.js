3044158f56428c916def7a9ee7dca44dd2b998bd
Roman Pavlovskyi <roman@pumpup.co>
2017-10-14 13:12:40 -0400

    Add form

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React, { Component } from 'react'
 import getWeb3 from './utils/getWeb3'
 import { ZeroEx } from '0x.js'
 import BigNumber from 'bignumber.js'
+import ReactQueryParams from 'react-query-params';
 
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
+import Form from './Form'
 
-class App extends Component {
+class App extends ReactQueryParams {
   constructor(props) {
     super(props)
 
     this.state = {
-      web3        : null,
-      zeroEx      : null,
-      signedOrder : null,
-      error       : null,
-      coinbase    : null,
+      web3           : null,
+      zeroEx         : null,
+      signedOrder    : null,
+      error          : null,
+
+      coinbase       : null,
+      transferAmount : 0,
+      toAddr         : '',
     }
+
+    this.handleSubmit       = this.handleSubmit.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleToAddrChange = this.handleToAddrChange.bind(this);
+
   }
 
 
@@ -62,21 +72,24 @@ class App extends Component {
 
   async makeOrder(coinbase, zeroEx) {
 
-    console.log('Coinbase 2: ', coinbase)
+    console.log('Params: ', JSON.stringify(this.queryParams))
+    const {
+      tokenAddr,
+    } = this.queryParams
 
     const orderHash = ZeroEx.getOrderHashHex({
-      exchangeContractAddress: '0x90fe2af704b34e0224bf2299c838e04d4dcf1364',
-      expirationUnixTimestampSec: '1508457600',
-      feeRecipient: '0x0000000000000000000000000000000000000000',
-      maker: coinbase,
-      makerFee: new BigNumber(0),
-      makerTokenAddress: '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
-      makerTokenAmount: new BigNumber(1),
-      salt: ZeroEx.generatePseudoRandomSalt(),
-      taker: ZeroEx.NULL_ADDRESS,
-      takerFee: new BigNumber(0),
-      takerTokenAddress: '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
-      takerTokenAmount: new BigNumber(1),
+      exchangeContractAddress    : '0x90fe2af704b34e0224bf2299c838e04d4dcf1364',
+      expirationUnixTimestampSec : '1508457600',
+      feeRecipient               : '0x0000000000000000000000000000000000000000',
+      maker                      : coinbase,
+      makerFee                   : new BigNumber(0),
+      makerTokenAddress          : '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
+      makerTokenAmount           : new BigNumber(1),
+      salt                       : ZeroEx.generatePseudoRandomSalt(),
+      taker                      : ZeroEx.NULL_ADDRESS,
+      takerFee                   : new BigNumber(0),
+      takerTokenAddress          : '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
+      takerTokenAmount           : new BigNumber(1),
     })
 
     return await zeroEx.signOrderHashAsync(orderHash, coinbase)
@@ -85,11 +98,24 @@ class App extends Component {
 
 
 
-  make() {
+  handleSubmit() {
+    const { transferAmount, toAddr } = this.state
 
+    console.log('Amount: ', transferAmount)
+    console.log('toAddr: ', toAddr)
   }
 
 
+
+  handleAmountChange(event) {
+    this.setState({ transferAmount: event.target.value });
+  }
+
+
+
+  handleToAddrChange(event) {
+    this.setState({ toAddr: event.target.value });
+  }
 
 
 
@@ -106,11 +132,47 @@ class App extends Component {
               {this.renderWeb3Info()}
               {this.renderOrderInfo()}
               {this.renderError()}
+              {this.renderTransferForm()}
             </div>
           </div>
         </main>
       </div>
     );
+  }
+
+
+
+  renderTransferForm() {
+
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <label>
+          To:
+          <input
+            type="text"
+            value={this.state.toAddr}
+            onChange={this.handleToAddrChange}
+            />
+        </label>
+        <br />
+        <label>
+          Amount:
+          <input
+            type="text"
+            value={this.state.transferAmount}
+            onChange={this.handleAmountChange}
+            />
+          {/* <select value={this.state.targetTokenName}>
+            <option selected value="eth">Grapefruit</option>
+            <option value="ethwaterloo">Lime</option>
+            <option value="gno">Coconut</option>
+            <option value="mango">Mango</option>
+          </select> */}
+        </label>
+        <input type="submit" value="Submit" />
+      </Form>
+    );
+
   }
 
 
---
+++ src/Form.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+
+
+export default ({ onSubmit, children}) =>
+  <form
+    onKeyDown={
+      (e) => {
+        /**
+         * Note: Pressing enter in some input in a browser forms
+         *  triggers onClick on the first child button
+         *
+         * So, prevent `enter` from triggering `onClick` on any buttons
+         *  and instead trigger onSubmit
+         */
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          onSubmit();
+        }
+      }
+    }
+
+    onSubmit={
+      (e) => {
+        /**
+         * Prevent submit from reloading the page
+         */
+        e.preventDefault();
+        e.stopPropagation();
+        onSubmit();
+      }
+    }>
+    {children}
+  </form>
