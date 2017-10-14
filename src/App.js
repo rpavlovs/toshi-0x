1933d19d150067804e93ffdf9356010fb040dfbe
Roman Pavlovskyi <roman@pumpup.co>
2017-10-14 05:39:55 -0400

    Add create order logic.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
+import { ZeroEx } from '0x.js'
+import BigNumber from 'bignumber.js'
 
 import './css/oswald.css'
 import './css/open-sans.css'
@@ -12,60 +13,70 @@ class App extends Component {
     super(props)
 
     this.state = {
-      storageValue: 0,
-      web3: null
+      web3        : null,
+      zeroEx      : null,
+      signedOrder : null,
+      error       : null,
     }
   }
 
-  componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
+  async componentWillMount() {
 
-    getWeb3
-    .then(results => {
+    try {
+
+      const { web3 } = await getWeb3
+      const zeroEx = new ZeroEx(web3.currentProvider)
+
+      console.log('Web3 version:', web3.version.api)
+      console.log('Coinbase', await web3.eth.coinbase)
+
       this.setState({
-        web3: results.web3
+        web3,
+        zeroEx,
       })
 
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
+      const signedOrder = await this.makeOrder()
+      this.setState({ signedOrder })
+    }
+    catch(error) {
+      console.log(error)
+      this.setState({ error })
+    }
+
   }
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
+  async makeOrder() {
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const { web3, zeroEx } = this.state
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+    const orderHash = ZeroEx.getOrderHashHex({
+      exchangeContractAddress: '0x90fe2af704b34e0224bf2299c838e04d4dcf1364',
+      expirationUnixTimestampSec: '1508457600',
+      feeRecipient: '0x0000000000000000000000000000000000000000',
+      maker: web3.eth.coinbase,
+      makerFee: new BigNumber(0),
+      makerTokenAddress: '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
+      makerTokenAmount: new BigNumber(1),
+      salt: '12345',
+      taker: '0x0000000000000000000000000000000000000000',
+      takerFee: new BigNumber(0),
+      takerTokenAddress: '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
+      takerTokenAmount: new BigNumber(1),
     })
+
+    return await zeroEx.signOrderHashAsync(orderHash, web3.eth.coinbase)
+
   }
+
+
+
+  make() {
+
+  }
+
+
+
+
 
   render() {
     return (
@@ -77,18 +88,52 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              {this.renderWeb3Info()}
+              {this.renderOrderInfo()}
+              {this.renderError()}
             </div>
           </div>
         </main>
       </div>
     );
   }
+
+
+
+  renderWeb3Info() {
+    const { web3 } = this.state
+
+    if (!web3) {
+      return <p>Web3 not found</p>
+    }
+    return (
+      <p>
+        {'Web3 version: ' + web3.version.api}
+        <br/>
+        {'Coinbase: ' + web3.eth.coinbase}
+      </p>
+    )
+  }
+
+
+
+  renderOrderInfo() {
+    return <p>{'Order: ' + JSON.stringify(this.state.signedOrder)}</p>
+  }
+
+
+
+  renderError() {
+    if (this.state.error) {
+      return (
+        <p style={{color: 'red'}}>
+          {'' + this.state.error}
+        </p>
+      )
+    }
+  }
+
+
 }
 
 export default App
