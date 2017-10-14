475efd6a2eb37ecdf3a03c665c281ef06729cd0a
Roman Pavlovskyi <roman@pumpup.co>
2017-10-14 07:15:42 -0400

    Upgrade web3. Use babel-env.

--- src/App.js
+++ src/App.js
@@ -17,8 +17,24 @@ class App extends Component {
       zeroEx      : null,
       signedOrder : null,
       error       : null,
+      coinbase    : null,
     }
   }
+
+
+
+  getCoinbase(web3) {
+    return new Promise((resolve, reject) => {
+      web3.eth.getCoinbase((error, coinbase) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(coinbase)
+      })
+    })
+  }
+
+
 
   async componentWillMount() {
 
@@ -26,17 +42,16 @@ class App extends Component {
 
       const { web3 } = await getWeb3
       const zeroEx = new ZeroEx(web3.currentProvider)
+      console.log('Web3 version: ', web3.version.api)
+      this.setState({ web3, zeroEx })
 
-      console.log('Web3 version:', web3.version.api)
-      console.log('Coinbase', await web3.eth.coinbase)
+      const coinbase = await this.getCoinbase(web3)
+      console.log('Coinbase: ', coinbase)
+      this.setState({ coinbase })
 
-      this.setState({
-        web3,
-        zeroEx,
-      })
-
-      const signedOrder = await this.makeOrder()
+      const signedOrder = await this.makeOrder(coinbase, zeroEx)
       this.setState({ signedOrder })
+
     }
     catch(error) {
       console.log(error)
@@ -45,26 +60,26 @@ class App extends Component {
 
   }
 
-  async makeOrder() {
+  async makeOrder(coinbase, zeroEx) {
 
-    const { web3, zeroEx } = this.state
+    console.log('Coinbase 2: ', coinbase)
 
     const orderHash = ZeroEx.getOrderHashHex({
       exchangeContractAddress: '0x90fe2af704b34e0224bf2299c838e04d4dcf1364',
       expirationUnixTimestampSec: '1508457600',
       feeRecipient: '0x0000000000000000000000000000000000000000',
-      maker: web3.eth.coinbase,
+      maker: coinbase,
       makerFee: new BigNumber(0),
       makerTokenAddress: '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
       makerTokenAmount: new BigNumber(1),
-      salt: '12345',
-      taker: '0x0000000000000000000000000000000000000000',
+      salt: ZeroEx.generatePseudoRandomSalt(),
+      taker: ZeroEx.NULL_ADDRESS,
       takerFee: new BigNumber(0),
       takerTokenAddress: '0x05d090b51c40b020eab3bfcb6a2dff130df22e9c',
       takerTokenAmount: new BigNumber(1),
     })
 
-    return await zeroEx.signOrderHashAsync(orderHash, web3.eth.coinbase)
+    return await zeroEx.signOrderHashAsync(orderHash, coinbase)
 
   }
 
@@ -101,7 +116,7 @@ class App extends Component {
 
 
   renderWeb3Info() {
-    const { web3 } = this.state
+    const { web3, coinbase } = this.state
 
     if (!web3) {
       return <p>Web3 not found</p>
@@ -110,7 +125,7 @@ class App extends Component {
       <p>
         {'Web3 version: ' + web3.version.api}
         <br/>
-        {'Coinbase: ' + web3.eth.coinbase}
+        {'Coinbase: ' + coinbase}
       </p>
     )
   }
